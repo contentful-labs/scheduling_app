d7fcab40cefcb1cc1f243dd3f50385c9b7e8c271
Gabe Tonon <gaberpt@gmail.com>
2015-11-05 15:39:51 -0200

    Add ordering parameter

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ activate :contentful do |f|
   f.access_token = "b99a46242f4a6e49263f30844bbb28649460e5b89088b97b3c79e14e6da12a8f"
   f.space = {magazine: "50gfvusg5ukj"}
   f.content_types = {article: "TE5C4G3m2AOwWcCoM6Cqc"}
-  f.cda_query = {content_type: "TE5C4G3m2AOwWcCoM6Cqc",'fields.endDateTime[gt]' => @timenow, 'fields.startDateTime[lte]' => @timenow}
+  f.cda_query = {content_type: "TE5C4G3m2AOwWcCoM6Cqc",'fields.endDateTime[gt]' => @timenow, 'fields.startDateTime[lte]' => @timenow, 'order' => 'sys.createdAt' }
 end
 
 set :css_dir, 'stylesheets'
